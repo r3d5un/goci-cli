551c703965a223af483cbe59b7ac83d3671b0f6d
r3d5un <r3d5un@pm.me>
2023-08-17 19:34:25 +0200

    Add formatting step to pipeline

--- main_test.go
+++ main_test.go
@@ -16,7 +16,7 @@ func TestRun(t *testing.T) {
 		{
 			name:   "success",
 			proj:   "./testdata/tool/",
-			out:    "Go Build: SUCCESS\nGo Test: SUCCESS\n",
+			out:    "Go Build: SUCCESS\nGo Test: SUCCESS\nGofmt: SUCCESS\n",
 			expErr: nil,
 		},
 		{
@@ -24,6 +24,12 @@ func TestRun(t *testing.T) {
 			proj:   "./testdata/toolErr/",
 			out:    "",
 			expErr: &stepErr{step: "go build"},
+		},
+		{
+			name:   "failFormat",
+			proj:   "./testdata/toolFmtErr/",
+			out:    "",
+			expErr: &stepErr{step: "go fmt"},
 		},
 	}
 
